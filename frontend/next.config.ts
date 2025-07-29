73ff2841005d7ce6e6610c7db645cf0c02ebb96f
shubhambhojane98 <shubhambhojane90@gmail.com>
2025-07-29 17:11:04 +0530

    setup proxy in next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // proxy this
+        destination: "http://localhost:5000/api/:path*", // to your Express backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
